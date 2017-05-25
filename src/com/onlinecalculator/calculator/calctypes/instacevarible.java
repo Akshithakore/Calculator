ec31d49c31d8705cd52e73839fc8c378ce66ac7b
istar <istar@192.168.100.10>
2017-05-25 18:05:29 +0530

    instance variable prog

---
+++ src/com/onlinecalculator/calculator/calctypes/instacevarible.java
@@ -0,0 +1,23 @@
+package com.onlinecalculator.calculator.calctypes;
+import java.util.*;
+ class instacevaribleone {
+        int x;
+      void display()
+      {
+    	  x=15;
+    	  Scanner sc=new Scanner(System.in);
+    	  
+    	 System.out.println("instance variable of x is.."+x);
+      }
+ }
+     public class instacevarible
+      {
+      public static void main(String args[])
+      {
+    	  instacevaribleone s=new instacevaribleone();
+    	  s.display();
+      }
+      
+	}
+
+
